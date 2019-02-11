e33bd024cf8607fed30257ef52d2ed3996c1a781
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-02-11 16:26:36 -0700

    Added friends array with test data.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,19 @@
 // Data saved in an array of objects
+const friends = [
+    {
+        "name": "Imaginary Friend",
+        "photo": "https://blogsimages.adobe.com/creativecloud/wp-content/themes/AdobeCreativeCloud/images/featured/feature_spacer.png"
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+        ]
+    }
+]
