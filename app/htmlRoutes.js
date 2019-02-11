162678c344fbb6e4e4c909707cf8877bcf6e757b
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-02-11 16:13:14 -0700

    Added routes to home and survey pages.

--- app/htmlRoutes.js
+++ app/htmlRoutes.js
@@ -1,3 +1,9 @@
 // GET route to /survery to display survery page
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
 
 // Default route to home page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
