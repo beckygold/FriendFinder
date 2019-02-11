ef31c2f463aba9997a7fb6f1b91738206b30952c
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-02-11 16:23:08 -0700

    Added POST route to friends array.

--- app/apiRoutes.js
+++ app/apiRoutes.js
@@ -4,6 +4,15 @@ app.get("/api/friends", function(req, res) {
 });
 
 // POST route to /api/friends to handle incoming survery results and compatibility logic
+app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+
+    console.log(newFriend);
+
+    friends.push(newFriend);
+
+    res.json(newFriend);
+});
 
     // compare difference between current user's scores against other users for each question 
     
