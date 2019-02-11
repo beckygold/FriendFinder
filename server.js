c25ca36a9188ac8afcc94939160f0d0848f88e5e
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-02-11 16:09:46 -0700

    added express and path dependencies. set up express app.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // Require NPM packages Express and Path
+const express = require("express");
+const path = require("path");
 
 // Set up Express server app and port
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Set up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Link to data
 
